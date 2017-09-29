0c08dcf8d3f368efebd2e7c021893746e1359c0a
raajponnam <raajponnam@gmail.com>
2017-09-29 16:38:59 +0100

    RomanNumeral 1 - 49

---
+++ RomanNumeralsTests/RomanNumerals.cs
@@ -0,0 +1,44 @@
+﻿namespace RomanNumeralsTests
+{
+    public class RomanNumerals
+    {
+        public int ConvertToNumber(string input)
+        {
+            return CountOfCharacters(input);
+        }
+
+        private int CountOfCharacters(string input)
+        {
+            var count = 0;
+            foreach (var ch in input)
+            {
+                switch (ch)
+                {
+                    case 'I':
+                        count++;
+                        break;
+                    case 'V':
+                        if (NeedToSubstract(count))
+                        {
+                            count -= 2;
+                        }
+                        count += 5;
+                        break;
+                    case 'X':
+                        if (NeedToSubstract(count))
+                        {
+                            count -= 2;
+                        }
+                        count += 10;
+                        break;
+                }
+            }
+            return count;
+        }
+
+        private bool NeedToSubstract(int count)
+        {
+            return count != 0 && ((count - 1) % 10 == 0 || (count - 1) % 5 == 0);
+        }
+    }
+}
---
+++ RomanNumeralsTests/RomanNumeralsTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumeralsTests
+{
+    [TestFixture]
+    public class RomanNumeralsTest
+    {
+        [TestCase("I", 1)]
+        [TestCase("II",2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("VII", 7)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        [TestCase("X", 10)]
+        [TestCase("XIII", 13)]
+        [TestCase("XIV",14)]
+        [TestCase("XVIII", 18)]
+        [TestCase("XIX", 19)]
+        [TestCase("XXXIX", 39)]
+
+        public void convert_roman_numerals_to_number_one(string input, int expected)
+        {
+            var romanNumerals = new RomanNumerals();
+            var result = romanNumerals.ConvertToNumber(input);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+    }
+}
