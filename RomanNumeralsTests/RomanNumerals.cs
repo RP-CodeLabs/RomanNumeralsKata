467c4164788cee8bb5410b74161373ffb9ba3818
raajponnam <raajponnam@gmail.com>
2017-10-02 15:31:53 +0100

    50 to 100 Roman numerals

--- RomanNumeralsTests/RomanNumerals.cs
+++ RomanNumeralsTests/RomanNumerals.cs
@@ -18,23 +18,31 @@
                         count++;
                         break;
                     case 'V':
-                        count = SubstractCount(count);
+                        count = SubstractCount(count, 2, NeedToSubstract(count));
                         count += 5;
                         break;
                     case 'X':
-                        count = SubstractCount(count);
+                        count = SubstractCount(count, 2, NeedToSubstract(count));
                         count += 10;
+                        break;
+                    case 'L':
+                        count = SubstractCount(count, 20, SubtractForLAndC(count));
+                        count += 50;
+                        break;
+                    case 'C':
+                        count = SubstractCount(count, 20, SubtractForLAndC(count));
+                        count += 100;
                         break;
                 }
             }
             return count;
         }
 
-        private int SubstractCount(int count)
+        private int SubstractCount(int count, int numberToSubtract, bool canSubtract)
         {
-            if (NeedToSubstract(count))
+            if (canSubtract)
             {
-                count -= 2;
+                count -= numberToSubtract;
             }
             return count;
         }
@@ -42,6 +50,11 @@
         private bool NeedToSubstract(int count)
         {
             return count != 0 && ((count - 1) % 10 == 0 || (count - 1) % 5 == 0);
+        }
+
+        private bool SubtractForLAndC(int count)
+        {
+            return count != 0 && (count - 1) % 9 == 0;
         }
     }
 }
